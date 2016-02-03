d961b8ea1b101d79e2338f692808bb6400c2fa4a
Pavel Davydov <typical000@gmail.com>
2016-02-03 13:07:31 +0200

    Add dot navigations functionality

--- simpleSlider.js
+++ simpleSlider.js
@@ -15,11 +15,16 @@
       viewportSelector: '.core-slider_viewport',
       itemSelector: '.core-slider_item',
       navEnabled: true, // Enable/Disable navigation arrows
-      navSelector: '.core-slider_nav',
-      navItemNextSelector: '.core-slider_arrow__right',
-      navItemPrevSelector: '.core-slider_arrow__left',
+      navSelector: '.core-slider_nav', // Selector for navigation arrows container
+      navItemNextSelector: '.core-slider_arrow__right', // 'Next' arrow selector
+      navItemPrevSelector: '.core-slider_arrow__left', // 'Prev' arrow selector
+      controlNavEnabled: false, // Enable/Disable control navigation (dots)
+      controlNavSelector: '.core-slider_control-nav', // Control navigation container selector (inside will be created dots items)
+      controlNavItemSelector: 'core-slider_control-nav-item', // Single control nav dot (created dynamically. Write without dot. If you need more that one class - add them with space separator)
       loadedClass: 'is-loaded', // Classname, that will be added when slider is fully loaded
       clonedClass: 'is-cloned', // Classname, that will be added to cloned slides (see option 'clone')
+      disabledClass: 'is-disabled', // Classname, that will be added it item is disabled (in most of cases - item will be display: noned)
+      activeClass: 'is-active', // Classname, that will be added to active items (for example control navs, etc.)
       reloadGif: false, // Reload gif's for replaying cycled animation in image
       clone: false, // Clone last 3 items of slider at the begin and at the end of slider
       items: 1, // How mutch items will be placed inside. Leave 1 if this is slider
@@ -36,8 +41,11 @@
         $slider = $sliderContainer.find(self.settings.sliderSelector),
         $sliderItems = $sliderContainer.find(self.settings.itemSelector),
         $clonedSliderItems = null,
+        $sliderNav = $sliderContainer.find(self.settings.navSelector),
         $sliderPrevBtn = $sliderContainer.find(self.settings.navItemPrevSelector),
         $sliderNextBtn = $sliderContainer.find(self.settings.navItemNextSelector),
+        $sliderControlNav = $sliderContainer.find(self.settings.controlNavSelector),
+        $sliderControlNavItems,
         slideCount = $sliderItems.length - 1,
         slideWidth, // Single slide width
         currentSlide = 0,
@@ -99,6 +107,44 @@
           self.stop();
         });
       }
+
+      // Add handlers for slider navs (prev/next)
+      if(self.settings.navEnabled) {
+        $sliderPrevBtn.on('click', function(){
+          self.setSlide(--currentSlide);
+        });
+        $sliderNextBtn.on('click', function(){
+          self.setSlide(++currentSlide);
+        });
+      } else {
+        // Add disabled class for navigration arrows
+        $sliderNav.addClass(self.settings.disabledClass);
+      }
+
+      // Add handlers and init slider control navs
+      if(self.settings.controlNavEnabled) {
+        // Create dynamically dot items and append them to container
+        var buffer = []; // Container of all dot items that will be created later
+        for(var i = 0; i < slideCount+1; i++) {
+          if(i == currentSlide) {
+            // Make current item active from begin
+            buffer.push('<div class="' + self.settings.controlNavItemSelector + ' ' + self.settings.activeClass + '"></div>');
+          } else {
+            buffer.push('<div class="' + self.settings.controlNavItemSelector + '"></div>');
+          }
+        }
+        $sliderControlNav.append(buffer.join(''));
+        // Cache all items in variable
+        $sliderControlNavItems = $sliderControlNav.children();
+        // Add event handlers to container
+        $sliderControlNav.on('click', $sliderControlNavItems, function(e){
+          currentSlide = $(e.target).index();
+          self.setSlide(currentSlide);
+        });
+      } else {
+        // Add disabled class for navigration dots
+        $sliderControlNav.addClass(self.settings.disabledClass);
+      }
     };
 
     this.cloneSlides = function(){
@@ -135,6 +181,11 @@
           $this.attr('src', currentUrl);
         });
       }
+      // Set active new control nav item
+      if(self.settings.controlNavEnabled && typeof $sliderControlNavItems !== 'undefined') {
+        $sliderControlNavItems.removeClass(self.settings.activeClass);
+        $sliderControlNavItems.eq(currentSlide).addClass(self.settings.activeClass);
+      }
       // Apply CSS transition to block
       $slider.css(transformPrefix, getTranslateX(-(currentSlide + self.settings.cloneItems) * slideWidth));
     };
@@ -169,15 +220,6 @@
     this.stop = function(){
       clearInterval(animateInterval);
     };
-
-    // Add handlers for slider navs
-    $sliderPrevBtn.on('click', function(){
-      self.setSlide(currentSlide--);
-    });
-
-    $sliderNextBtn.on('click', function(){
-      self.setSlide(currentSlide++);
-    });
 
     // Finally init slider
     this.init();
